748dfc00bfe33d1154ee704d9d93a65af20decd1
Al-Amin Miah <alaminmiah95@hotmail.co.uk>
2021-08-26 15:25:39 +0100

    initial commit for state practice

---
+++ statepractice/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Game from './game';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className="App">
+      <Game />
+    </div>;
+  }
+}
+
+
+export default App;
