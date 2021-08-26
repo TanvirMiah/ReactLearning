17cf36b533723015eafbb2d69844f95486c52ee6
Al-Amin Miah <alaminmiah95@hotmail.co.uk>
2021-08-26 16:58:24 +0100

    State Practise with buttons

--- statepractice/src/App.js
+++ statepractice/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import Game from './game';
+import Game from './Game';
+import Rando from './Rando';
+import Button from './Button';
 import './App.css';
+import BrokenClick from './BrokenClick';
 
 class App extends Component {
   render() {
     return <div className="App">
-      <Game />
+      <BrokenClick />
     </div>;
   }
 }
